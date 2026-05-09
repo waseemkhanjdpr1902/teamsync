c28d35895ccdd32f39afb57593e28d51d93e3444
waseemkhanjdpr1902 <waseemkhanjdpr@gmail.com>
2026-05-09 18:33:16 +0530

    Update vite.config.js

--- apps/web/vite.config.js
+++ apps/web/vite.config.js
@@ -6,11 +6,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  build: {
-    outDir: "../../dist/apps/web",
-    emptyOutDir: true,
-  },
+      "@": path.resolve(__dirname, "./src")
+    }
+  }
 });
